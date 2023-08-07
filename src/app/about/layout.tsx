e40d3967fdbb87248b7c8e65adf770e42d2c0b14
Yu Suzuki_nire <suzuki-yuu@nire.work>
2023-08-07 14:54:08 +0900

    first-commit

---
+++ src/app/about/layout.tsx
@@ -0,0 +1,11 @@
+export default function AboutLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            {children}
+        </div>
+    )
+}
